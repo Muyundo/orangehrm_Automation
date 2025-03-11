48237a329ea7ab39dff10a969cb141df88fc61b7
Muyundo <muyundobrian@gmail.com>
2025-03-12 00:32:27 +0300

    included login

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -26,3 +26,10 @@
 Cypress.Commands.add('baseurl', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/')
 })
+
+Cypress.Commands.add('login', () =>{
+        cy.get('input[name = "username"]').type('Admin')
+        cy.get('input[name = password]').type('admin123')
+        cy.get('.oxd-button').contains('Login').click()
+   
+})
