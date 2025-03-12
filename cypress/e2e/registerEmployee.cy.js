f69c4788842597775ad1ac9f428187a247f200bc
Muyundo <muyundobrian@gmail.com>
2025-03-12 17:25:08 +0300

    save details

--- cypress/e2e/registerEmployee.cy.js
+++ cypress/e2e/registerEmployee.cy.js
@@ -10,4 +10,14 @@ it('Register employee', () =>{
     cy.get ('.oxd-button').contains('Add')
       .click()
 
-})
+    cy.get('input[name = "firstName"]', {timeout: 10000}).type('Gorge')
+    cy.get('input[name = "middleName"]').type('Gorge')
+    cy.get('input[name = "lastName"]').type('Gorge')
+    cy.get('.oxd-button--secondary').contains('Save')
+      .click()
+
+
+      })
+      
+
+
