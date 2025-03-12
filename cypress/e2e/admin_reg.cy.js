7bfc904ba864c62deb19966f786b2a40de5ad64f
Muyundo <muyundobrian@gmail.com>
2025-03-12 17:27:41 +0300

    admin

--- cypress/e2e/admin_reg.cy.js
+++ cypress/e2e/admin_reg.cy.js
@@ -4,9 +4,20 @@ beforeEach(() =>{
     cy.login()
     
     })
-    it('Login page', () => {
+    it('Add new user', () => {
         cy.get('.oxd-navbar-nav').contains('Admin').click()
-    })
+        cy.get('.orangehrm-header-container > .oxd-button').click()
+        cy.get('div.oxd-select-wrapper .oxd-select-text.oxd-select-text--active').contains('-- Select --')
+          .click()
+        cy.get('.oxd-select-dropdown').contains('ESS').click()
+
+        cy.get('div.oxd-select-wrapper .oxd-select-text.oxd-select-text--active .oxd-select-text-input', {timeout: 10000})
+          .contains('-- Select --')
+          .click()
+          cy.get('.oxd-select-dropdown').contains('Enabled').click()
+
+        cy.get('.oxd-autocomplete-text-input--before').type('George')
+})
      
     
     
