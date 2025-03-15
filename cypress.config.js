af9ac51c81347ada18e8a244b42b607cd1ab34dc
Muyundo <muyundobrian@gmail.com>
2025-03-15 12:03:49 +0300

    added project id

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    projectId: "y4wd1o",
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
