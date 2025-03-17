44384a5ec0ba00140f5e9497d5fa3b87bc7c5130
Muyundo <muyundobrian@gmail.com>
2025-03-17 14:09:55 +0300

    added faker.js

---
+++ cypress/support/faker.js
@@ -0,0 +1,9 @@
+import { faker } from '@faker-js/faker'
+
+export const randomInputs = {
+    fname: faker.person.firstName(),
+    mname: faker.person.middleName(),
+    lname: faker.person.lastName(),
+
+    
+}
