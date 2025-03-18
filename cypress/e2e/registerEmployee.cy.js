a29521ef91a50f55c9a1e8eef9614b3171c27fda
Muyundo <muyundobrian@gmail.com>
2025-03-18 15:04:03 +0300

    updated random number generation

--- cypress/e2e/registerEmployee.cy.js
+++ cypress/e2e/registerEmployee.cy.js
@@ -1,5 +1,7 @@
 ///  <reference types= "cypress" />
 import { randomInputs } from '../support/faker'
+const randomNumber = Math.floor(100000 + Math.random() * 900000)
+
 
 beforeEach(() =>{
     cy.baseurl()
@@ -15,6 +17,7 @@ it('Register employee', () =>{
     cy.get('input[name = "firstName"]', {timeout: 10000}).type(randomInputs.fname)
     cy.get('input[name = "middleName"]').type(randomInputs.mname)
     cy.get('input[name = "lastName"]').type(randomInputs.lname)
+    cy.get('.oxd-grid-item > .oxd-input-group > :nth-child(2) > .oxd-input').type(randomNumber)
     cy.get('.oxd-file-input').selectFile('cypress/fixtures/canva/profile.png', {force: true})
     cy.get('.oxd-button--secondary').contains('Save')
       .click()
