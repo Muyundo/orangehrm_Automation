b27521557ca7c8405c08b7201cf1a3a2cb0a7384
Muyundo <muyundobrian@gmail.com>
2025-03-18 11:06:31 +0300

    added client registration verification

--- cypress/e2e/registerEmployee.cy.js
+++ cypress/e2e/registerEmployee.cy.js
@@ -17,7 +17,9 @@ it('Register employee', () =>{
     cy.get('input[name = "lastName"]').type(randomInputs.lname)
     cy.get('.oxd-button--secondary').contains('Save')
       .click()
-      cy.get('.oxd-toast-container.oxd-toast-container--bottom').should('contain', 'Successfully Saved');
+    cy.get('.oxd-toast-container.oxd-toast-container--bottom').should('contain', 'Successfully Saved')
+    cy.url().should('include', '/pim/viewPersonalDetails')
+
 
 
 
