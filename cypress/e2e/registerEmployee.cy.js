8a1f7e941899129834d18218d2ba53898a44ec33
Muyundo <muyundobrian@gmail.com>
2025-03-19 14:34:17 +0300

    added form validation

--- cypress/e2e/registerEmployee.cy.js
+++ cypress/e2e/registerEmployee.cy.js
@@ -14,7 +14,7 @@ it('Register employee', () =>{
     cy.get ('.oxd-button').contains('Add')
       .click()
 
-    cy.get('input[name = "firstName"]', {timeout: 10000}).type(randomInputs.fname)
+    cy.get('input[name = "firstName"]').type(randomInputs.fname)
     cy.get('input[name = "middleName"]').type(randomInputs.mname)
     cy.get('input[name = "lastName"]').type(randomInputs.lname)
     cy.get('.oxd-grid-item > .oxd-input-group > :nth-child(2) > .oxd-input').type(randomNumber)
@@ -25,6 +25,23 @@ it('Register employee', () =>{
     cy.url().should('include', '/pim/viewPersonalDetails')
 
       })
+
+it('Check for mandatory fields',() => {
+    cy.get('.oxd-navbar-nav').contains('PIM')
+      .click()
+    cy.get ('.oxd-button').contains('Add')
+      .click()
+
+    cy.get('.oxd-button--secondary').contains('Save')
+      .click()
+    cy.get('input[name = "firstName"]').parent().parent().find('.oxd-input-field-error-message')
+      .contains('Required')
+      .should('be.visible')
+    cy.get('input[name = "lastName"]').parent().parent().find('.oxd-input-field-error-message')
+      .contains('Required')
+      .should('be.visible')
+    
+})
       
 
 
