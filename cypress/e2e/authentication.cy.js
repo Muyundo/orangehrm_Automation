06edecfcf9aa5517168efe39a233f25ad5c8f79c
MUYUNDO BRIAN <muyundobrian@gmail.com>
2025-06-17 08:26:31 +0300

    added timeout for stability

--- cypress/e2e/authentication.cy.js
+++ cypress/e2e/authentication.cy.js
@@ -8,7 +8,7 @@ it('Load login page', () => {
 })
 
 it('login using Incorrect username and correct password', () =>{
-    cy.get('input[name = "username"]').type('Incorrect username')
+    cy.get('input[name = "username"]', {timeout: 10000}).type('Incorrect username')
     cy.get('input[name = password]').type('admin123')
     cy.get('.oxd-button').contains('Login').click()
     cy.get('.oxd-alert').should('contain', 'Invalid credentials', {timeout: 10000}).should('be.visible')
@@ -16,7 +16,7 @@ it('login using Incorrect username and correct password', () =>{
 })
 
     it('login using correct username and incorrect password', () =>{
-        cy.get('input[name = "username"]').type('Admin')
+        cy.get('input[name = "username"]', {timeout: 10000}).type('Admin')
         cy.get('input[name = password]').type('incorrect password')
         cy.get('.oxd-button').contains('Login').click()
         cy.get('.oxd-alert').should('contain', 'Invalid credentials', {timeout: 10000}).should('be.visible')
@@ -25,13 +25,13 @@ it('login using Incorrect username and correct password', () =>{
     })
     
     it('login without username and password', () =>{
-        cy.get('.oxd-button').contains('Login').click()
+        cy.get('.oxd-button', {timeout: 10000}).contains('Login').click()
         cy.get('.oxd-form').contains('Required', {timeout: 10000}).should('be.visible')
 
     })
     
     it('login using correct username and correct password', () =>{
-        cy.get('input[name = "username"]').type('Admin')
+        cy.get('input[name = "username"]', {timeout: 10000}).type('Admin')
         cy.get('input[name = password]').type('admin123')
         cy.get('.oxd-button').contains('Login').click()
         cy.url().should('include', '/dashboard/index')
