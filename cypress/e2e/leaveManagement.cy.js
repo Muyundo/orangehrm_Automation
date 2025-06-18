e391403e509a9243aaa5c8f3f47ad6df2a64c79f
MUYUNDO BRIAN <muyundobrian@gmail.com>
2025-06-18 07:53:25 +0300

    added scrip to verify that leave application page loads successfully

--- cypress/e2e/leaveManagement.cy.js
+++ cypress/e2e/leaveManagement.cy.js
@@ -6,8 +6,16 @@ beforeEach(() => {
     cy.baseurl()
     cy.login()
 })
-it('Verify that leave application page is loaded', () => {
+/*it('Verify that leave application page is loaded', () => {
     cy.get('.oxd-main-menu-item', {timeout: 10000}).contains('Leave')
       .click()
     cy.url().should('include', '/leave/viewLeaveList')
+})*/
+it('Apply for leave', () => {
+    cy.get('.oxd-main-menu-item', {timeout: 10000}).contains('Leave')
+      .click()
+    cy.get('.oxd-topbar-body').contains('Apply')
+      .click()
+      cy.url().should('include', '/leave/applyLeave')
+ 
 })
