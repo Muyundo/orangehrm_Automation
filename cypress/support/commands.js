b0eebbb3bb46f78f663c08f79b6bd677f024c8bd
MUYUNDO BRIAN <muyundobrian@gmail.com>
2025-06-19 07:57:52 +0300

    upddated date selection for leave

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -15,4 +15,59 @@ Cypress.Commands.add('login', () =>{
    
 })
 
+
+Cypress.Commands.add('datePicker', () => {
+  const dayjs = require('dayjs')
+
+  let startDate
+  while (true) {
+    const offset = Math.floor(Math.random() * 14)
+    const candidate = today.add(offset, 'day');
+    if (candidate.day() >= 1 && candidate.day() <= 4) {
+      startDate = candidate
+      break
+    }
+  }
+
+  let leaveDays = 0;
+  let endDate = startDate;
+  while (leaveDays < 4) {
+    if (endDate.day() !== 0 && endDate.day() !== 6) leaveDays++;
+    if (leaveDays < 4) endDate = endDate.add(1, 'day');
+  }
+
+  const selectDate = (label, date) => {
+    const day = date.date();
+    const month = date.format('MMMM')
+    const year = date.year();
+
+    cy.contains('label', label)
+      .parents('.oxd-input-group')
+      .find('.oxd-date-input-icon')
+      .click();
+
+    cy.get('.oxd-calendar-selector-month').then($el => {
+      if (!$el.text().includes(month)) {
+        const now = dayjs();
+        const diff = date.diff(now, 'month');
+        const arrowSelector = diff > 0 ? '.bi-chevron-right' : '.bi-chevron-left';
+
+        for (let i = 0; i < Math.abs(diff); i++) {
+          cy.get(arrowSelector).click();
+        }
+      }
+    });
+
+    cy.get('.oxd-calendar-date')
+      .not('.oxd-calendar-date--disabled')
+      .contains(new RegExp(`^${day}$`))
+      .click();
+  };
+
+  selectDate('From Date', startDate);
+  selectDate('To Date', endDate);
+});
+
+
+
    
