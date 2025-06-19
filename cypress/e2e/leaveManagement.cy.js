399113df1fc1dff7fbf01958a30a04ca9aa537d2
MUYUNDO BRIAN <muyundobrian@gmail.com>
2025-06-19 07:58:32 +0300

    Updated date function

--- cypress/e2e/leaveManagement.cy.js
+++ cypress/e2e/leaveManagement.cy.js
@@ -1,21 +1,33 @@
 /// <reference types="cypress" />
-import { randomInputs } from '../support/faker'
 const randomNumber = Math.floor(100000 + Math.random() * 900000)
 
 beforeEach(() => {
     cy.baseurl()
     cy.login()
 })
-/*it('Verify that leave application page is loaded', () => {
+it('Verify that leave application page is loaded', () => {
     cy.get('.oxd-main-menu-item', {timeout: 10000}).contains('Leave')
       .click()
     cy.url().should('include', '/leave/viewLeaveList')
-})*/
+})
 it('Apply for leave', () => {
     cy.get('.oxd-main-menu-item', {timeout: 10000}).contains('Leave')
       .click()
-    cy.get('.oxd-topbar-body').contains('Apply')
+    cy.get('.oxd-topbar-body', {timeout:10000})
+      .contains('Apply')
       .click()
-      cy.url().should('include', '/leave/applyLeave')
+    cy.url().should('include', '/leave/applyLeave')
+    cy.get('.oxd-select-text').click()
+    cy.get('.oxd-select-dropdown').contains('CAN - Bereavement').click()
+    cy.datePicker()
+    cy.get('body').type('{esc}')
+    
+    cy.get('.oxd-textarea').type('Testing leave application for 4 weekdays');
+    cy.get('.oxd-button--secondary').contains('Apply')
+        .click();
+    cy.get('.oxd-toast-container.oxd-toast-container--bottom', {timeout: 10000}).should('contain', 'Successfully Applied');
+    cy.url().should('include', '/leave/viewLeaveList')
+
+
  
 })
